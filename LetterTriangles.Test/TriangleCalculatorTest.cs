86d9b624cb5f1bf0ef0174d3dfe65e03ce666a15
jesse <jessefortuin850@gmail.com>
2023-06-08 12:46:12 +0200

    Feat: Created Test setup, .gitignore, README.md

---
+++ LetterTriangles.Test/TriangleCalculatorTest.cs
@@ -0,0 +1,25 @@
+namespace LetterTriangles.Test
+{
+    public class TriangleCalculatorTest
+    {
+        [Theory]
+        [InlineData("codewars", "l")]
+        [InlineData("triangle", "d")]
+        [InlineData("youhavechosentotranslatethiskata", "a")]
+        [InlineData("b", "b")]
+        [InlineData("yes ", "b")]
+        [InlineData("hours ", "y")]
+        [InlineData("circlecipher", "z")]
+        public void TriangleTests(string input, string expected)
+        {
+            //arrange
+            ITriangleCalculator calculator = new TriangleCalculator();
+
+            //act
+            string actual = calculator.Triangle(input);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
